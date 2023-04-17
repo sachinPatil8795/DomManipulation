5048dae1de8b53a341888336201ed1be9d95a33d
sachin_sp95 <sachinsp8795@gmail.com>
2023-04-17 16:46:01 +0530

    Styling list-Items with ClassName and tagName

--- dom.js
+++ dom.js
@@ -13,17 +13,29 @@ console.log(head);
 headerTitle.style.borderBottom = "solid 3px #000";
 
 //Styling Add Item
-const item = document.querySelector(".title");
-item.style.fontWeight = "bold";
-item.style.color = "green";
+const item = document.getElementById('addItem');
+item.style.fontWeight = 'bold';
+item.style.color = 'green';
 
+//Styling list-elements with getElementByClassName
 var items = document.getElementsByClassName("list-group-item");
 console.log(items);
+console.log(items[1]);
+items[1].style.backgroundColor = 'yellow';
 
-//Styling List-Items
+//Adding styling to array of elements with className
+for (let i = 0; i < items.length; i++) {
+    // item[i].style.backgroundColor = 'violet';
+    items[i].style.color = 'blue';
+    items[i].style.fontWeight = 'bold';
+  }
+
+//Styling List-Items using ElementsByTagName
 var listItems = document.getElementsByTagName("li");
 console.log(listItems);
 console.log(listItems[1]);
+listItems[1].style.backgroundColor = 'purple'
+
 
 //Making 3rd element background Color green
 listItems[2].style.backgroundColor = "green";
@@ -31,4 +43,7 @@ listItems[2].style.backgroundColor = "green";
 //making all the elements in the list have bold color font
 for (let i = 0; i < listItems.length; i++) {
   listItems[i].style.fontWeight = "bold";
+  listItems[i].style.color = 'black';
+  listItems[i].style.backgroundColor = 'grey';
 }
+
