323b3d235f5e152fa0b6d87b7b71cddc2632c573
sachin_sp95 <sachinsp8795@gmail.com>
2023-04-17 15:58:39 +0530

    Styling done for list-Items

--- dom.js
+++ dom.js
@@ -1,8 +1,8 @@
-//manipulating the title of the page 
-const headerTitle = document.getElementById('header-title');
+//manipulating the title of the page
+const headerTitle = document.getElementById("header-title");
 console.log(headerTitle);
 
-const head = document.getElementById('main-header');
+const head = document.getElementById("main-header");
 console.log(head);
 
 // headerTitle.innerHTML = '<h3>Love Coding</h3>';
@@ -10,9 +10,25 @@ console.log(head);
 // headerTitle.innerText = 'I am a CODER';
 
 //styling title
-headerTitle.style.borderBottom = 'solid 3px #000';
+headerTitle.style.borderBottom = "solid 3px #000";
 
 //Styling Add Item
-const item = document.querySelector('.title');
-item.style.fontWeight = 'bold';
-item.style.color = 'green';
+const item = document.querySelector(".title");
+item.style.fontWeight = "bold";
+item.style.color = "green";
+
+var items = document.getElementsByClassName("list-group-item");
+console.log(items);
+
+//Styling List-Items
+var listItems = document.getElementsByTagName("li");
+console.log(listItems);
+console.log(listItems[1]);
+
+//Making 3rd element background Color green
+listItems[2].style.backgroundColor = "green";
+
+//making all the elements in the list have bold color font
+for (let i = 0; i < listItems.length; i++) {
+  listItems[i].style.fontWeight = "bold";
+}
