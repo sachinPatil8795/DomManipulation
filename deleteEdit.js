05290c2c717fa84fb34879524a36af6fb22622b9
sachin_sp95 <sachinsp8795@gmail.com>
2023-04-20 16:02:31 +0530

    Adding filter Item List functionality

--- deleteEdit.js
+++ deleteEdit.js
@@ -8,12 +8,16 @@ form.addEventListener("submit", addItem);
 // Delete event
 itemList.addEventListener("click", removeItem);
 
+// Filter event
+filter.addEventListener("keyup", filterItems);
+
 // Add item function
 function addItem(e) {
   e.preventDefault();
 
   // Get input value
   var newItem = document.getElementById("item").value;
+  var newItem2 = document.getElementById("item-description").value;
 
   // Create new li element
   var li = document.createElement("li");
@@ -23,6 +27,7 @@ function addItem(e) {
 
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(newItem2));
 
   // Create delete button element
   var deleteBtn = document.createElement("button");
@@ -55,4 +60,42 @@ function removeItem(e) {
       itemList.removeChild(li);
     }
   }
+}
+
+// Filter Items
+function filterItems(e) {
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+
+  // Get list
+  var items = itemList.getElementsByTagName("li");
+
+  // Convert to an array
+  Array.from(items).forEach(function (item) {
+    var itemName = item.firstChild.textContent;
+    if (itemName.toLowerCase().indexOf(text) != -1) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+
+  //filter item based on description
+
+  // convert text to lowercase
+  var text1 = e.target.value.toLowerCase();
+
+  // Get list
+  var itemDescription = itemList.getElementsByTagName("li");
+  console.log({ itemDescription: itemDescription });
+  // Convert to an array
+  Array.from(itemDescription).forEach(function (item1) {
+    var itemNameDes = item1.textContent;
+
+    if (itemNameDes.toLowerCase().indexOf(text1) != -1) {
+      item1.style.display = "block";
+    } else {
+      item1.style.display = "none";
+    }
+  });
 }
