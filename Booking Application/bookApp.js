c11d122ab8daf6170034ea4d0b1de68b045067c6
sachin_sp95 <sachinsp8795@gmail.com>
2023-04-23 21:55:27 +0530

    Booking Application done

---
+++ Booking Application/bookApp.js
@@ -0,0 +1,22 @@
+//
+function getCallDetails(event){
+    event.preventDefault();
+    const name = document.getElementById('fname').value;
+    const email = document.getElementById('myEmail').value;
+    const phoneNo = document.getElementById('myPhone').value;
+    const date = document.getElementById('myDate').value;
+    const time = document.getElementById('myTime').value;
+
+    const randomId = Math.random().toString(16).slice(2);
+    console.log(randomId);
+    
+    const myDetails = {
+        Name : name,
+        Email : email,
+        PhoneNo : phoneNo,
+        Date : date,
+        Time : time,
+        Id : randomId
+    }
+    localStorage.setItem(randomId,JSON.stringify(myDetails));
+}
