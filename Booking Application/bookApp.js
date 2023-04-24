e79e6c768a38723be960a9a4ae8658ff5518555d
sachin_sp95 <sachinsp8795@gmail.com>
2023-04-24 21:27:10 +0530

    Lets Scale the app to more users

--- Booking Application/bookApp.js
+++ Booking Application/bookApp.js
@@ -1,22 +1,45 @@
-//
-function getCallDetails(event){
-    event.preventDefault();
-    const name = document.getElementById('fname').value;
-    const email = document.getElementById('myEmail').value;
-    const phoneNo = document.getElementById('myPhone').value;
-    const date = document.getElementById('myDate').value;
-    const time = document.getElementById('myTime').value;
+//Getting all the details
+function getCallDetails(event) {
+  event.preventDefault();
+  const name = document.getElementById("fname").value;
+  const email = document.getElementById("myEmail").value;
+  const phoneNo = document.getElementById("myPhone").value;
+  const date = document.getElementById("myDate").value;
+  const time = document.getElementById("myTime").value;
 
-    const randomId = Math.random().toString(16).slice(2); //Random key value pairs
-    console.log(randomId);
-    
-    const myDetails = {
-        Name : name,
-        Email : email,
-        PhoneNo : phoneNo,
-        Date : date,
-        Time : time,
-        Id : randomId
-    }
-    localStorage.setItem(randomId,JSON.stringify(myDetails));
+  const randomId = Math.random().toString(16).slice(2); //Random key value pairs
+  console.log(randomId);
+
+  const myDetails = {
+    Name: name,
+    Email: email,
+    PhoneNo: phoneNo,
+    Date: date,
+    Time: time,
+    Id: randomId,
+  };
+  localStorage.setItem(randomId, JSON.stringify(myDetails));
 }
+
+function getAllkeys(){
+  const myKey = Object.keys(localStorage);
+  return myKey;
+  
+}
+
+//Display the content of the local storage on an HTML page
+function DisplayKeyValue(){
+  const myValues = getAllkeys();
+  const ul = document.getElementById('myVal');
+  
+
+  for(let i = 0; i < myValues.length; i++){
+    const val = JSON.parse(localStorage.getItem(myValues[i]));
+    const li = document.createElement('li');
+
+    li.innerHTML = `${val.Name} ${val.Email} ${val.PhoneNo} ${val.Date} ${val.Time}`
+    ul.appendChild(li);
+    
+  }
+}
+DisplayKeyValue();
